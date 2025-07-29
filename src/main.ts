1732f96570281ea33e6bb3c16bf3587009b5ba9b
chidubemovbiebo <chidubemovbiebo@gmail.com>
2025-07-29 07:17:11 +0100

    Added latest substack post

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ window.Alpine = Alpine;
 Alpine.data("posts", () => ({
   posts: [
     {
+      name: "Logical Clocks - Beyond Lamport Clocks",
+      date: "02/07/2024",
+      url: "https://chidubemovbiebo.substack.com/p/logical-clocks-beyond-lamport-clocks",
+    },
+    {
       name: "Logical Clocks - Ordering Distributed Events Without Timekeeping",
       date: "03/10/2024",
       url: "https://chidubemovbiebo.substack.com/p/logical-clocks-introduction",
